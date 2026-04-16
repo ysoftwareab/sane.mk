e0a7ee8dc0fc2d0095072482b16781c79555a5e6
Andrei Neculau <andrei.neculau@gmail.com>
2026-04-16 20:00:00 +0200

    add node python

--- exe.misc.inc.mk
+++ exe.misc.inc.mk
@@ -7,7 +7,8 @@ FIND_Q_NOSYM = $(FIND_Q) -L
 GREP_FILENAME = $(GREP) -rl
 LS_ALL = $(LS) -A
 MVBAK = $(MV) --backup=numbered
-$(foreach VAR,CPBAK CP_NOSYM EDITOR FIND_Q FIND_Q_NOSYM GREP_FILENAME LS_ALL MVBAK,$(call make-lazy,$(VAR)))
+PATCH_STDOUT = $(PATCH) -o -
+$(foreach VAR,CPBAK CP_NOSYM EDITOR FIND_Q FIND_Q_NOSYM GREP_FILENAME LS_ALL MVBAK PATCH_STDOUT,$(call make-lazy,$(VAR)))
 $(foreach VAR,DIFF_SS,$(call make-lazy-once,$(VAR)))
 
 COLUMN = $(call which,COLUMN,column)
@@ -18,17 +19,42 @@ JQ = $(call which,JQ,jq)
 YQ = $(call which,YQ,yq)
 $(foreach VAR,COLUMN CURL HEXDUMP JD JQ YQ,$(call make-lazy,$(VAR)))
 
+# git
 GIT = $(call which,GIT,git)
 GIT_LS = $(GIT) ls-files
 GIT_LS_NEW = $(GIT_LS) --others --directory --no-empty-directory
 GIT_LS_SUB = $(CAT) .gitmodules | $(GREP) "path =" | $(SED) "s/.\{0,\}path = //"
 $(foreach VAR,GIT GIT_LS GIT_LS_NEW GIT_LS_SUB,$(call make-lazy,$(VAR)))
 
-PATCH_STDOUT = $(PATCH) -o -
+# node
+export N_PREFIX = $(TMPDIR)
+N = $(call which,N,n)
+NODE = $(call which,NODE,node)
+NPX = $(call which,NPX,npx)
+
+# node corepack
+COREPACK = $(call which,COREPACK,corepack)
+ifeq (COREPACK_NOT_FOUND,$(COREPACK))
+COREPACK = $(NPX) --yes corepack
+endif
+ifneq (,$(PKG_PACKAGE_MANAGER))
+ifneq ($(PKG_PACKAGE_MANAGER),$(patsubst npm@%,%,$(PKG_PACKAGE_MANAGER)))
+NPM = $(COREPACK) npm
+endif
+ifneq ($(PKG_PACKAGE_MANAGER),$(patsubst pnpm@%,%,$(PKG_PACKAGE_MANAGER)))
+PNPM = $(COREPACK) pnpm
+endif
+endif
+
+# python
+PYTHON ?= $(call which,PYTHON,python)
+PYTHON3 ?= $(call which,PYTHON3,python3)
+
+# zip
 UNZIP = $(call which,UNZIP,unzip) -oq
 UNZIP_Z1 = $(call which,UNZIP,unzip) -Z1
 VISUAL ?= $(EDITOR)
 ZIP_NOSYM = $(call which,ZIP_NOSYM,zip) -r
 ZIP = $(ZIP_NOSYM) -y
 ZIPINFO = $(call which,ZIPINFO,zipinfo)
-$(foreach VAR,PATCH_STDOUT UNZIP UNZIP_Z1 VISUAL ZIP_NOSYM ZIP ZIPINFO,$(call make-lazy,$(VAR)))
+$(foreach VAR,UNZIP UNZIP_Z1 VISUAL ZIP_NOSYM ZIP ZIPINFO,$(call make-lazy,$(VAR)))
