2ecc6360321481435548194193cf879858d86cce
Andrei Neculau <andrei.neculau@gmail.com>
2026-04-17 21:00:00 +0200

    add debug

---
+++ extra/debug.env.inc.mk
@@ -0,0 +1,76 @@
+# ------------------------------------------------------------------------------
+
+.PHONY: debug/env
+debug/env:
+	$(ECHO)
+	$(ECHO_DO) "Debug env..."
+	$(ECHO) "BASH=$(BASH)"
+	$(ECHO) "CI=$(CI)"
+	$(ECHO) "DOCKER=$(DOCKER)"
+	$(ECHO) "HOST=$(HOST)"
+	$(ECHO) "LANG=$(LANG)"
+	$(ECHO) "LC_ALL=$(LC_ALL)"
+	$(ECHO) "MAKE=$(MAKE)"
+	$(ECHO) "SHELL=$(SHELL)"
+	$(ECHO) "TZ=$(TZ)"
+	$(ECHO) "USER=$(USER)"
+	$(ECHO) "V=$(V)"
+	$(ECHO) "VERBOSE=$(VERBOSE)"
+	$(ECHO_DONE)
+	$(ECHO)
+	$(ECHO_DO) "Debug env (paths)..."
+	$(ECHO) "GIT_ROOT=$(GIT_ROOT)"
+	$(ECHO) "HOME=$(HOME)"
+	$(ECHO) "MAKE_PATH=$(MAKE_PATH)"
+	$(ECHO) "PATH=$(PATH)"
+	$(ECHO) "PWD=$(PWD)"
+	$(ECHO) "SANE_MK_ROOT=$(SANE_MK_ROOT)"
+	$(ECHO_DONE)
+
+
+.PHONY: debug/env/docker
+debug/env/docker:
+	$(ECHO)
+	$(ECHO_DO) "Debug env (Docker)..."
+	$(ECHO) "DOCKER=$(DOCKER)"
+	set -x && $(DOCKER) --version || true
+	set -x && $(DOCKER) version || true
+	set -x && $(DOCKER) info || true
+	set -x && $(DOCKER) buildx version || true
+	set -x && $(DOCKER) buildx ls || true
+	set -x && $(DOCKER) buildx inspect --bootstrap || true
+	set -x && $(DOCKER) compose version || true
+	$(ECHO_DONE)
+
+
+.PHONY: debug/env/homebrew
+debug/env/homebrew:
+	$(ECHO)
+	$(ECHO_DO) "Debug env (Homebrew)..."
+	$(ECHO) "BREW=$(BREW)"
+	$(ECHO) "HOMEBREW_PREFIX=$(HOMEBREW_PREFIX)"
+	set -x && $(BREW) --version || true
+	set -x && $(BREW) doctor || true
+	$(ECHO_DONE)
+
+
+.PHONY: debug/env/node
+debug/env/node:
+	$(ECHO)
+	$(ECHO_DO) "Debug env (NodeJS)..."
+	$(ECHO) "N=$(N)"
+	$(ECHO) "NODE=$(NODE)"
+	set -x && $(N) --version || true
+	set -x && $(NODE) --version || true
+	$(ECHO)
+
+
+.PHONY: debug/env/python
+debug/env/python:
+	$(ECHO)
+	$(ECHO_DO) "Debug env (Python)..."
+	$(ECHO) "PYTHON=$(PYTHON)"
+	$(ECHO) "PYTHON3=$(PYTHON3)"
+	set -x && $(PYTHON) --version || true
+	set -x && $(PYTHON3) --version || true
+	$(ECHO_DONE)
--- misc.os.inc.mk
+++ misc.os.inc.mk
@@ -19,3 +19,46 @@ $(foreach VAR,ARCH ARCH_NORMALIZED ARCH_SHORT ARCH_BIT,$(call make-lazy,$(VAR)))
 OS = $(shell $(UNAME) | $(TR) "[:upper:]" "[:lower:]")
 OS_SHORT = $(shell $(ECHO) $(OS) | $(SED) "s/^\([[:alpha:]]\{1,\}\).*\$$/\1/g")
 $(foreach VAR,OS OS_SHORT,$(call make-lazy,$(VAR)))
+
+# ------------------------------------------------------------------------------
+
+.PHONY: debug/os
+debug/os:
+	$(ECHO)
+	$(ECHO_DO) "Debug variables (OS)..."
+	$(ECHO) "ARCH=$(ARCH)"
+	$(ECHO) "ARCH_BIT=$(ARCH_BIT)"
+	$(ECHO) "ARCH_NORMALIZED=$(ARCH_NORMALIZED)"
+	$(ECHO) "ARCH_SHORT=$(ARCH_SHORT)"
+	$(ECHO) "OS=$(OS)"
+	$(ECHO) "OS_SHORT=$(OS_SHORT)"
+	$(ECHO_DONE)
+
+
+.PHONY: debug/os/darwin
+debug/os/darwin:
+	$(ECHO)
+	$(ECHO_DO) "Debug OS (Darwin)..."
+	set -x && /usr/bin/sw_vers || true
+	set -x && /usr/bin/xcodebuild -version || true
+	set -x && /usr/bin/xcode-select --print-path || true
+	set -x && /usr/bin/xcrun --version || true
+#	set -x && /usr/sbin/system_profiler || true # too slow, so output only some data types
+	set -x && /usr/sbin/system_profiler SPSoftwareDataType || true
+	set -x && /usr/sbin/system_profiler SPDeveloperToolsDataType || true
+	set -x && /usr/sbin/system_profiler SPHardwareDataType || true
+	set -x && /usr/sbin/system_profiler SPMemoryDataType || true
+	set -x && /usr/sbin/system_profiler SPStorageDataType || true
+	$(ECHO_DONE)
+
+
+.PHONY: debug/os/linux
+debug/os/linux:
+	$(ECHO)
+	$(ECHO_DO) "Debug OS (Linux)..."
+	set -x && /usr/bin/lsb_release -a || true
+	set -x && cat /etc/os-release || true
+	set -x && uname -a || true
+	set -x && df -h || true
+	set -x && free -h || true
+	$(ECHO_DONE)
