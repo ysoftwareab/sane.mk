378c7cdbffe7fbcc5f1253e3de4fc35cc19e137f
Andrei Neculau <andrei.neculau@gmail.com>
2026-04-25 20:13:17 +0200

    create sane.extended.mk

---
+++ sane.extended.mk
@@ -0,0 +1,40 @@
+ifndef SANE_MK_INCLUDED
+
+ifndef SANE_MK_DIR
+SANE_MK_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+endif
+
+include $(SANE_MK_DIR)/sane.mk
+
+include $(SANE_MK_DIR)/exe.which.inc.mk # needed by the other exe.*.inc.mk files
+include $(SANE_MK_DIR)/exe.echo.inc.mk
+include $(SANE_MK_DIR)/exe.gnu.inc.mk
+include $(SANE_MK_DIR)/exe.misc.inc.mk
+
+include $(SANE_MK_DIR)/chars.inc.mk
+include $(SANE_MK_DIR)/git.inc.mk
+include $(SANE_MK_DIR)/os.inc.mk
+
+include $(SANE_MK_DIR)/target.env.inc.mk
+include $(SANE_MK_DIR)/target.help.inc.mk
+include $(SANE_MK_DIR)/target.lazy.inc.mk
+include $(SANE_MK_DIR)/target.noop.inc.mk
+include $(SANE_MK_DIR)/target.printvar.inc.mk
+include $(SANE_MK_DIR)/target.verbose.inc.mk
+
+MAKEFILE_LAZY ?= true
+ifeq (true,$(MAKEFILE_LAZY))
+ifeq ($(MAKECMDGOALS),$(filter-out %Makefile.lazy,$(MAKECMDGOALS)))
+ifeq (,$(wildcard Makefile.lazy))
+$(info [DO  ] Generating Makefile.lazy...)
+$(info $(shell $(MAKE) Makefile.lazy))
+$(info [DONE])
+$(info )
+endif
+include Makefile.lazy
+endif
+endif
+
+SANE_MK_INCLUDED := 1
+
+endif
