dbe0aba4723971848a93060eb3ce33624e05e05e
Andrei Neculau <andrei.neculau@gmail.com>
2026-04-27 22:00:00 +0200

    disable ai-slop-detector

--- sane.extra.mk
+++ sane.extra.mk
@@ -40,7 +40,6 @@ include $(SANE_MK_ROOT)/extra/deps.uv.inc.mk
 include $(SANE_MK_ROOT)/extra/deps.vscode.inc.mk
 
 include $(SANE_MK_ROOT)/extra/check.actionlint.inc.mk
-include $(SANE_MK_ROOT)/extra/check.ai-slop-detector.inc.mk
 include $(SANE_MK_ROOT)/extra/check.black.inc.mk
 include $(SANE_MK_ROOT)/extra/check.editorconfig-checker.inc.mk
 include $(SANE_MK_ROOT)/extra/check.jscpd.inc.mk
@@ -60,7 +59,6 @@ include $(SANE_MK_ROOT)/misc.lazy.inc.mk
 
 SANE_DEBUG_CHECK += \
 	debug/brew \
-	debug/ai-slop-detector \
 	debug/black	\
 	debug/editorconfig-checker \
 	debug/jscpd \
