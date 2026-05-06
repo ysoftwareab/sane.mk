4112e768b720320eb528f0ff902fb41c83627441
Andrei Neculau <andrei.neculau@gmail.com>
2026-05-06 21:00:00 +0200

    GIT_COMMON_DIR is enough

--- extra/deps.git.inc.mk
+++ extra/deps.git.inc.mk
@@ -1,8 +1,17 @@
 # ------------------------------------------------------------------------------
 
+$(GIT_COMMON_DIR)/info/attributes: $(SANE_MK_ROOT)/gitconfig/dot.gitattributes_global
+	$(MKDIR) $(dir $@)
+	$(LN) -s $< $@
+
+
+$(GIT_COMMON_DIR)/info/exclude: $(SANE_MK_ROOT)/gitconfig/dot.gitignore_global
+	$(MKDIR) $(dir $@)
+	$(LN) -s $< $@
+
+
 .PHONY: deps/git
+deps/git: $(GIT_COMMON_DIR)/info/attributes
+deps/git: $(GIT_COMMON_DIR)/info/exclude
 deps/git:
-	$(LN) -s $(SANE_MK_ROOT)/config/dot.gitattributes_global $(GIT_DIR)/info/attributes || \
-		$(LN) -s $(SANE_MK_ROOT)/config/dot.gitattributes_global $(GIT_COMMON_DIR)/info/attributes
-	$(LN) -s $(SANE_MK_ROOT)/config/dot.gitignore_global $(GIT_DIR)/info/exclude || \
-		$(LN) -s $(SANE_MK_ROOT)/config/dot.gitignore_global $(GIT_COMMON_DIR)/info/exclude
+	:
