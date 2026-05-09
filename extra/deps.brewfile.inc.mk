bb35dcf5bec89603ae6d6d95b24a7788c6ad1b50
Andrei Neculau <andrei.neculau@gmail.com>
2026-05-09 23:00:00 +0200

    add check/system/brewfile

--- extra/deps.brewfile.inc.mk
+++ extra/deps.brewfile.inc.mk
@@ -6,21 +6,25 @@ BREWFILE = Brewfile
 BREWFILE_SH = Brewfile.sh
 BREWFILE_TEST = Brewfile.test
 
+CAT_BREWFILE = \
+	$(CAT) $(BREWFILE) \
+		| $(SED) "s/\$${?ARCH}?/$(ARCH)/g" \
+		| $(SED) "s/\$${?ARCH_SHORT}?/$(ARCH_SHORT)/g" \
+		| $(SED) "s/\$${?HOST}?/$(HOST)/g" \
+		| $(SED) "s/\$${?HOST_SHORT}?/$(HOST_SHORT)/g" \
+		| $(SED) "s/\$${?OS_SHORT}?/$(OS_SHORT)/g" \
+		| $(SED) "s/\$${?OS}?/$(OS)/g" \
+		| $(SED) "s/^$(hash) source \\(.\\+\\)$$/cat \1/e" \
+		| $(SED) "s/^$(hash) source \\(.\\+\\)$$/cat \1/e" \
+		| $(SED) "s/^$(hash) source \\(.\\+\\)$$/cat \1/e"
+
 # ------------------------------------------------------------------------------
 
 .PHONY: deps/system/brewfile
 deps/system/brewfile:
 ifneq (,$(wildcard $(BREWFILE)))
 	unset GITHUB_ACTIONS; \
-	$(CAT) $(BREWFILE) \
-		| $(SED) "s/\${?ARCH_SHORT}?/$(ARCH_SHORT)/g" \
-		| $(SED) "s/\${?ARCH}?/$(ARCH)/g" \
-		| $(SED) "s/\${?HOST}?/$(HOST)/g" \
-		| $(SED) "s/\${?OS_SHORT}?/$(OS_SHORT)/g" \
-		| $(SED) "s/\${?OS}?/$(OS)/g" \
-		| $(SED) "s/^# source (.+)$/cat \1/e" \
-		| $(SED) "s/^# source (.+)$/cat \1/e" \
-		| $(SED) "s/^# source (.+)$/cat \1/e" \
+	$(CAT_BREWFILE) \
 		| brew bundle install --verbose --file=-
 ifneq (,$(wildcard $(BREWFILE_SH)))
 	OS=$(OS) OS_SHORT=$(OS_SHORT) ARCH=$(ARCH) ARCH_SHORT=$(ARCH_SHORT) HOST=$(HOST) ./$(BREWFILE_SH)
@@ -32,8 +36,13 @@ endif
 
 .PHONY: test/system/brewfile
 test/system/brewfile:
+ifneq (,$(wildcard $(BREWFILE)))
+	unset GITHUB_ACTIONS; \
+	$(CAT_BREWFILE) \
+		| brew bundle check --verbose --file=-
 ifneq (,$(wildcard $(BREWFILE_TEST)))
 	OS=$(OS) OS_SHORT=$(OS_SHORT) ARCH=$(ARCH) ARCH_SHORT=$(ARCH_SHORT) HOST=$(HOST) ./$(BREWFILE_TEST)
+endif
 else
 	:
 endif
