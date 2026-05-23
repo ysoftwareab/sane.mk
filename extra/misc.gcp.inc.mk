6f047b470f0c9d9342e351146581504d14ee51cb
Andrei Neculau <andrei.neculau@gmail.com>
2026-05-23 18:34:31 +0200

    add gcp impersonate sa

--- extra/misc.gcp.inc.mk
+++ extra/misc.gcp.inc.mk
@@ -3,26 +3,36 @@ GOOGLE_CLOUD_PROJECT_ID ?= $(shell $(GCLOUD) config get-value project 2>/dev/nul
 # ------------------------------------------------------------------------------
 
 .PHONY: gcp/auth
-gcp/auth: ## Authenticate with GCP (Application Default Credentials).
+gcp/auth: ## Authenticate with GCP.
 	$(GCLOUD) auth application-default login
+
+
+.PHONY: gcp/auth/%
+gcp/auth/%: ## Authenticate with GCP as a service account.
+	$(GCLOUD) auth application-default login --impersonate-service-account=$*
+
 
 .PHONY: gcp/auth/quota
 gcp/auth-quota: ## Set quota project for Application Default Credentials.
 	$(GCLOUD) auth application-default set-quota-project $(GOOGLE_CLOUD_PROJECT_ID)
 
+
 .PHONY: gcp/project/name
 gcp/project/name: ## Show current GCP project name.
 	$(GCLOUD) config get-value project
+
 
 .PHONY: gcp/project
 gcp/project: gcp/project/name
 gcp/project: ## Show current GCP project info.
 	$(GCLOUD) projects describe --format=json $(GOOGLE_CLOUD_PROJECT_ID)
 
+
 .PHONY: gcp/services
 gcp/services: gcp/project/name
 gcp/services: ## List enabled GCP APIs/services.
 	$(GCLOUD) services list --format=json --enabled --project $(GOOGLE_CLOUD_PROJECT_ID)
+
 
 .PHONY: gcp/services/all
 gcp/services/all: gcp/project/name
