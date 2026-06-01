1a64263b3c3a4c28d2de99bfcf7345280847db10
Andrei Neculau <andrei.neculau@gmail.com>
2026-06-01 09:11:06 +0200

    fix ECHO_INDENT

--- exe.echo.inc.mk
+++ exe.echo.inc.mk
@@ -4,7 +4,7 @@
 ECHO2 ?= $(ECHO) $$($(DATE) "+%H:%M:%S")
 ECHO_DO = $(ECHO2) -- "[DO  ]"
 ECHO_DONE = $(ECHO2) -- "[DONE]"
-ECHO_INDENT = $(ECHO) -n $$($(DATE) "+%H:%M:%S") -- "      " | $(SED) "s/./ /g" && $(ECHO)
+ECHO_INDENT = $(ECHO) -n "$$($(DATE) "+%H:%M:%S") -- [XXXX] " | $(SED) "s/./ /g" && $(ECHO)
 ECHO_NEXT = $(ECHO2) -- "[NEXT]"
 ECHO_Q = $(ECHO2) -- "[Q   ]"
 ECHO_SKIP = $(ECHO2) -- "[SKIP]"
