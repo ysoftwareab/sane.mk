ad568d3a8281788d6cde6023a7352c3d20a3253d
Andrei Neculau <andrei.neculau@gmail.com>
2026-06-08 15:56:40 +0200

    ruff of course changes the root when you pass --config. ofc...

--- extra/check.ruff.inc.mk
+++ extra/check.ruff.inc.mk
@@ -26,14 +26,6 @@ RUFF_FILES_SHEBANG = $(PYTHON_FILES_SHEBANG)
 RUFF_FILES_FILTER_OUT += \
 	$(DEFAULT_FILES_FILTER_OUT) \
 
-RUFF_CONFIG = $(GIT_ROOT)/.ruff.toml
-ifeq (,$(wildcard $(RUFF_CONFIG)))
-RUFF_CONFIG = $(GIT_ROOT)/ruff.toml
-endif
-ifneq (,$(wildcard $(RUFF_CONFIG)))
-RUFF_FLAGS += --config $(RUFF_CONFIG)
-endif
-
 # ------------------------------------------------------------------------------
 
 .PHONY: debug/ruff
